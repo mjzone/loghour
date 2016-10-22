0cedf2366978e4be195ab0028fa511b314c28748
ashanfernando <ashan256@gmail.com>
2016-10-22 21:35:32 +0530

    Fix environmental variables not loading issue in lambda

--- serverless/handlers/authorize/index.js
+++ serverless/handlers/authorize/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var parser = require('../parser'),
-    request = require('request');
+    request = require('request'),
+    env = require('dotenv').config();
 
 module.exports.handler = (event, context, cb) => {
     var event = parser.parseEvent(event);
-    request('https://github.com/login/oauth/access_token?client_id=' + process.env.GITHUB_CLIENT_ID + '&client_secret=' + process.env.GITHUB_CLIENT_SECRET + '&code=' + event.params.code,
+    request('https://github.com/login/oauth/access_token?client_id=' + process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET + '&code=' + event.params.code,
         function(error, response, body) {
             if (!error && response.statusCode == 200 && body.indexOf("access_token") !== -1) {
                 context.succeed({
