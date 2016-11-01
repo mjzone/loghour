0c0d096ac108f7d83e8d851457434ee5e6e4f43a
ashanfernando <ashan256@gmail.com>
2016-11-01 14:12:12 +0530

    Fixng the repo access permission scope issue

--- web/src/components/login/LoginPage.js
+++ web/src/components/login/LoginPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 class LoginPage extends React.Component{
     render() {
-      let githubURL = "https://github.com/login/oauth/authorize?client_id="+ GLOBALS.github_id +"&scope=repos%20read:org";
+      let githubURL = "https://github.com/login/oauth/authorize?client_id="+ GLOBALS.github_id +"&scope=repo%20read:org";
         return (
             <div className="container-fluid">
                 <div className="jumbotron">
